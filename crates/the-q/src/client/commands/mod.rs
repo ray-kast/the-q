1a6fdc391dc83e2531405214de6d0b40bef61a5f
raykast <me@june-s.net>
2026-01-15 16:10:41 -0800

    Remove test commands.

--- crates/the-q/src/client/commands/mod.rs
+++ crates/the-q/src/client/commands/mod.rs
@@ -1,11 +1,8 @@
-mod explode;
 mod jpeg;
 mod liquid;
-mod point;
 mod re;
 mod say;
 mod sound;
-mod test;
 
 mod prelude {
     #![expect(unused_imports, reason = "Some exports may not yet be used")]
@@ -61,35 +58,29 @@ pub struct CommandOpts {
 pub fn handlers(opts: &CommandOpts) -> Handlers {
     use prelude::*;
 
-    let explode = Arc::new(explode::ExplodeCommand::from(opts));
     let jpeg = Arc::new(jpeg::JpegCommand::from(opts));
     let jpeg_message = Arc::new(jpeg::JpegMessageCommand::from(opts));
     let jpeg_user = Arc::new(jpeg::JpegUserCommand::from(opts));
     let liquid = Arc::new(liquid::LiquidCommand::from(opts));
     let liquid_message = Arc::new(liquid::LiquidMessageCommand::from(opts));
     let liquid_user = Arc::new(liquid::LiquidUserCommand::from(opts));
-    let point = Arc::new(point::PointCommand::from(opts));
     let re = Arc::new(re::ReCommand::from(opts));
     let re_message = Arc::new(re::ReMessageCommand::from(opts));
     let say = Arc::new(say::SayCommand::from(opts));
     let sound = Arc::new(sound::SoundCommand::from(opts));
-    let test = Arc::new(test::TestCommand::from(opts));
 
     Handlers {
         commands: vec![
             Arc::clone(&sound) as Arc<dyn CommandHandler<Schema>>,
-            explode,
             jpeg,
             jpeg_message,
             jpeg_user,
             liquid,
             liquid_message,
             liquid_user,
-            point,
             re,
             re_message,
             say,
-            test,
         ],
         components: vec![sound],
         modals: vec![],
