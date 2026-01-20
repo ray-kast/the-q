b1df31e47f9af79908f4af39605917bbb85d5951
raykast <me@june-s.net>
2026-01-19 22:48:49 -0800

    Update saturation logic for better edge cases.

--- crates/the-q/src/client/commands/saturate.rs
+++ crates/the-q/src/client/commands/saturate.rs
@@ -6,32 +6,72 @@ use crate::util;
 const MIN_PERCENT: f64 = 0.0;
 const MAX_PERCENT: f64 = 1_000.0;
 
-#[expect(clippy::unnecessary_wraps, reason = "Expected for closure signature")]
+const REINHARD_GAIN: f32 = 7.0;
+
+fn pseudo_reinhard(x: f32) -> f32 {
+    f32::from(x > 0.0) * (REINHARD_GAIN * x / ((REINHARD_GAIN - 1.0) * x + 1.0))
+}
+
+fn pseudo_reinhard_inv(x: f32) -> f32 {
+    f32::from(x > 0.0) * (x / (REINHARD_GAIN - (REINHARD_GAIN - 1.0) * x))
+}
+
+const fn luma_dropoff(x: f32) -> f32 {
+    const fn sigmoid(mut x: f32) -> f32 {
+        x -= 1.0;
+        0.5 - (x / (2.0 * x.abs() + 1.0))
+    }
+
+    sigmoid(x) / sigmoid(0.0)
+}
+
 fn saturate(image: DynamicImage, percent: Option<f64>) -> Result<DynamicImage, jpeggr::Error> {
     let percent @ MIN_PERCENT..=MAX_PERCENT = percent.unwrap_or(300.0) else {
         unreachable!();
     };
 
     #[expect(clippy::cast_possible_truncation)]
-    let factor = (percent / 100.0) as f32;
+    let saturation = (percent / 100.0) as f32;
 
     let mut buf = image.into_rgba32f();
+    buf.apply_color_space(
+        image::metadata::Cicp::SRGB_LINEAR,
+        image::ConvertColorOptions::default(),
+    )?;
 
+    let desat_amount = saturation.clamp(1.0, 2.0) - 1.0;
     for image::Rgba([r, g, b, _a]) in buf.pixels_mut() {
-        let mut lab = oklab::srgb_f32_to_oklab(oklab::Rgb {
-            r: *r,
-            g: *g,
-            b: *b,
+        let mut lab = oklab::linear_srgb_to_oklab(oklab::LinearRgb {
+            r: pseudo_reinhard_inv(*r),
+            g: pseudo_reinhard_inv(*g),
+            b: pseudo_reinhard_inv(*b),
         });
+
+        let desat = luma_dropoff(lab.l * 1.65);
+        let factor = saturation * (1.0 + desat_amount * (desat - 1.0));
+        #[cfg(debug_assertions)]
+        {
+            let factor_naive =
+                (1.0 - desat_amount) * saturation + desat_amount * desat * saturation;
+            assert!(
+                (factor - factor_naive).abs() < 1e-5,
+                "factor={factor} vs. factor_naive={factor_naive}"
+            );
+        }
+
         lab.a *= factor;
         lab.b *= factor;
-        oklab::Rgb {
-            r: *r,
-            g: *g,
-            b: *b,
-        } = oklab::oklab_to_srgb_f32(lab);
+
+        let out = oklab::oklab_to_linear_srgb(lab);
+        *r = pseudo_reinhard(out.r);
+        *g = pseudo_reinhard(out.g);
+        *b = pseudo_reinhard(out.b);
     }
 
+    buf.apply_color_space(
+        image::metadata::Cicp::SRGB,
+        image::ConvertColorOptions::default(),
+    )?;
     Ok(buf.into())
 }
 
@@ -51,10 +91,10 @@ impl From<&CommandOpts> for SaturateCommand {
 #[async_trait]
 impl CommandHandler<Schema> for SaturateCommand {
     fn register_global(&self) -> CommandInfo {
-        CommandInfo::build_slash(&self.name, "Content-aware scales an image", |a| {
+        CommandInfo::build_slash(&self.name, "Adjusts the saturation of an image", |a| {
             a.attachment("image", "The input image", true).real(
-                "x-percent",
-                "X resize percentage (defaults to Y percent or 50%)",
+                "percent",
+                "Saturation percentage (defaults to 300%)",
                 false,
                 MIN_PERCENT..=MAX_PERCENT,
             )
